a1053919a4177429bdca44bf2e9c1551f577a3b1
ping <513011631@qq.com>
2018-11-16 14:18:44 +0800

    修复点击颜色区无法绘画的bug,设置自行调节画笔及橡皮粗细,设置清空及保存后自动跳回绘画状态

--- canvas.js
+++ canvas.js
@@ -1,18 +1,23 @@
-var canvas=document.getElementById('canvas')
-console.log(1)
-var context=canvas.getContext('2d')
+var canvas = document.getElementById('canvas')
+var context = canvas.getContext('2d')
 var pageWidth = document.documentElement.clientWidth
 var pageHeight = document.documentElement.clientHeight
-var lineWidth = 2
- var lastPoint = {
-     "x": undefined,
-     "y": undefined
- }
- var using = false
- var eraserEnabled = false
+var range = document.getElementById("range");
+var event = new Event('click')
+var lineWidth = range.value
+var lastPoint = {
+    "x": undefined,
+    "y": undefined
+}
+var using = false
+var eraserEnabled = false
+//切换画笔粗细功能
+range.onchange = function () {
+    lineWidth = this.value;
+}
 autoSetCanvas(canvas, pageWidth, pageHeight)
 //设置canvas的宽和高
-function autoSetCanvas(canvas,x,y) {
+function autoSetCanvas(canvas, x, y) {
     setCanvasSize()
     window.onresize = function () {
         setCanvasSize()
@@ -21,119 +26,119 @@ function autoSetCanvas(canvas,x,y) {
         canvas.width = x
         canvas.height = y
     }
-}  
+}
 
 //非触屏设备
-    //监听鼠标事件
+//监听鼠标事件
 listenToMouse(canvas)
 function listenToMouse(canvas) {
     //切换功能
     canvasEraser.onclick = function () {
         eraserEnabled = true
     }
-    canvasThin.onclick = function () {
-        eraserEnabled = false
-    }
     canvasThick.onclick = function () {
         eraserEnabled = false
     }
     //特性检测
-    if (document.body.ontouchstart !== undefined&&document.body.ontouchstart!==null) {
-        //触屏设备
-        canvas.ontouchstart = function (aaa) {
-            var x = aaa.touches[0].clientX
-            var y = aaa.touches[0].clientY
-            using = true
-            if (eraserEnabled) {
-                context.clearRect(x - 3, y - 3, 6, 6)
-            } else {
-                lastPoint = {
-                    "x": x,
-                    "y": y
-                }
-                drawCircle(x, y, (lineWidth / 2))
-            }
-        }
-
-        canvas.ontouchmove = function (aaa) {
-            var x = aaa.touches[0].clientX
-            var y = aaa.touches[0].clientY
-            if (using == false) {
-                return {}
-            }
-            if (eraserEnabled) {
-                context.clearRect(x - 3, y - 3, 6, 6)
-            } else {
-                var newPoint = {
-                    "x": x,
-                    "y": y
-                }
-                drawCircle(x, y, (lineWidth / 2))
-                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, lineWidth)
-                lastPoint = newPoint
-            }
-        }
-
-        canvas.ontouchend = function () {
-            using = false
-        }
-
-        
+    if (document.body.ontouchstart !== undefined && document.body.ontouchstart !== null) {
+        touchDevice()
     } else {
-        //非触控设备,鼠标事件
-        canvas.onmousedown = function (aaa) {
-            var x = aaa.clientX
-            var y = aaa.clientY
-            using = true
-            if (eraserEnabled) {
-                context.clearRect(x - 3, y - 3, 6, 6)
-            } else {
-                lastPoint = {
-                    "x": x,
-                    "y": y
-                }
-                drawCircle(x, y, (lineWidth / 2))
-            }
-        }
-
-        canvas.onmousemove = function (aaa) {
-            var x = aaa.clientX
-            var y = aaa.clientY
-            if (using == false) {
-                return{}
-            }
-            if (eraserEnabled) {
-                context.clearRect(x - 3, y - 3, 6, 6)
-            } else {
-                var newPoint = {
-                    "x": x,
-                    "y": y
-                }
-                drawCircle(x, y, (lineWidth / 2))
-                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, lineWidth)
-                lastPoint = newPoint
-            }
-        }
-        canvas.onmouseup = function () {
-            using = false
-        }
+        unTouchDevice()
     }
 }
 
+function touchDevice() {
+    //触屏设备
+    canvas.ontouchstart = function (aaa) {
+        var x = aaa.touches[0].clientX
+        var y = aaa.touches[0].clientY
+        using = true
+        if (eraserEnabled) {
+            context.clearRect(x - 3, y - 3, lineWidth, lineWidth)
+        } else {
+            lastPoint = {
+                "x": x,
+                "y": y
+            }
+            drawCircle(x, y, (lineWidth / 2))
+        }
+    }
 
+    canvas.ontouchmove = function (aaa) {
+        var x = aaa.touches[0].clientX
+        var y = aaa.touches[0].clientY
+        if (using == false) {
+            return {}
+        }
+        if (eraserEnabled) {
+            context.clearRect(x - 3, y - 3, lineWidth, lineWidth)
+        } else {
+            var newPoint = {
+                "x": x,
+                "y": y
+            }
+            drawCircle(x, y, (lineWidth / 2))
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, lineWidth)
+            lastPoint = newPoint
+        }
+    }
+
+    canvas.ontouchend = function () {
+        using = false
+    }
+}
+function unTouchDevice() {
+    //非触控设备,鼠标事件
+    canvas.onmousedown = function (aaa) {
+        var x = aaa.clientX
+        var y = aaa.clientY
+        using = true
+        if (eraserEnabled) {
+            context.clearRect(x - 3, y - 3, lineWidth, lineWidth)
+        } else {
+            lastPoint = {
+                "x": x,
+                "y": y
+            }
+            drawCircle(x, y, (lineWidth / 2))
+        }
+    }
+
+    canvas.onmousemove = function (aaa) {
+        var x = aaa.clientX
+        var y = aaa.clientY
+        if (using == false) {
+            return {}
+        }
+        if (eraserEnabled) {
+            context.clearRect(x - 3, y - 3, lineWidth, lineWidth)
+        } else {
+            var newPoint = {
+                "x": x,
+                "y": y
+            }
+            drawCircle(x, y, (lineWidth / 2))
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, lineWidth)
+            lastPoint = newPoint
+        }
+    }
+    canvas.onmouseup = function () {
+        using = false
+    }
+}
 
 //画点和线
 function drawLine(x1, y1, x2, y2, lineWidth) {
     context.beginPath();
-    context.moveTo(x1,y1)
+    context.moveTo(x1, y1)
     context.lineWidth = lineWidth
-    context.lineTo(x2,y2)
+    context.lineTo(x2, y2)
     context.stroke()
     context.closePath()
 }
-function drawCircle(x,y,radius) {
+function drawCircle(x, y, radius) {
     context.beginPath();
-    context.arc(x,y,radius,0,Math.PI*2)  
+    context.arc(x, y, radius, 0, Math.PI * 2)
     context.fill()
 }
 
@@ -143,63 +148,78 @@ for (let index = 0; index < arr.length; index++) {
     arr[index].addEventListener("click", checked, false)
     function checked() {
         for (let index = 0; index < arr.length; index++) {
-            arr[index].classList.remove('checked')     
+            arr[index].classList.remove('checked')
         }
         arr[index].classList.add('checked')
-    } 
+    }
 }
 
 //清空页面功能
-canvasDelete.onclick=function () {
-    if (window.confirm("Do you really want to clear it?")) { 
+canvasDelete.onclick = function () {
+    if (window.confirm("Do you really want to clear it?")) {
         context.clearRect(0, 0, pageWidth, pageHeight)
+        setTimeout(() => {
+            document.querySelector('#canvasThick').dispatchEvent(event)
+        }, 1000); 
     }
     
 }
-//切换画笔粗细功能
-canvasThin.addEventListener("click", function () {lineWidth = 2;}, false)
-canvasThick.addEventListener("click", function () {lineWidth = 6;}, false)
+
 //保存功能
-canvasSave.onclick=function(){
-    //这里保存前须定义文件名....................................................................................................
-    var a =document.createElement('a')
+canvasSave.onclick = function () {
+    var a = document.createElement('a')
     document.body.appendChild(a)
-    var url=canvas.toDataURL("img/png")
-    a.href=url
-    a.download='newImage'
-    a.target='_blank'
+    var url = canvas.toDataURL("img/png")
+    a.href = url
+    a.download = 'newImage'
+    a.target = '_blank'
     a.click()
+    setTimeout(() => {
+        document.querySelector('#canvasThick').dispatchEvent(event)
+    }, 1000);
 }
 
 //颜色    待优化 这里默认点击后切到铅笔,用到事件委托???
-red.onclick=function(){
-    context.fillStyle = 'red'
-    context.strokeStyle='red'
+var colorWrapper = document.querySelector('.colorWrapper')
+    colorWrapper.addEventListener('click', changeColor, false)
+function changeColor(e) {
+    var color = e.target.id
+    context.fillStyle = color
+    context.strokeStyle = color
+    document.querySelector('#canvasThick').dispatchEvent(event)
 }
-yellow.onclick = function () {
-    context.fillStyle = 'yellow'
-    context.strokeStyle = 'yellow'
-}
-blue.onclick = function () {
-    context.fillStyle = 'blue'
-    context.strokeStyle = 'blue'
-}
-black.onclick = function () {
-    context.fillStyle = 'black'
-    context.strokeStyle = 'black'
-}
-green.onclick = function () {
-    context.fillStyle = 'green'
-    context.strokeStyle = 'green'
-}
-var colorList= document.querySelectorAll('.color')
-for(let index=0;index<colorList.length;index++){
-    colorList[index].addEventListener("click", changeColor, false)
-    function changeColor(){
+
+
+var colorList = document.querySelectorAll('.color')
+for (let index = 0; index < colorList.length; index++) {
+    colorList[index].addEventListener("click", changeActive, false)  
+    function changeActive() {
         for (let index = 0; index < colorList.length; index++) {
             colorList[index].classList.remove('active')
         }
         colorList[index].classList.add('active')
     }
 }
-console.log(3)
+
+
+
+// //布置的撤销功能
+// canvas.ontouchstart = function (e) {
+//     // 在这里储存绘图表面
+//     this.firstDot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+//     saveData(this.firstDot);
+
+// }
+
+// let undo = document.getElementById("undo");
+// let historyDeta = [];
+
+// function saveData (data) {
+//     (historyDeta.length === 10) && (historyDeta.shift()); // 上限为储存10步，太多了怕挂掉
+//     historyDeta.push(data);
+// }
+// undo.onclick = function(){
+//     if(historyDeta.length < 1) return false;
+//     ctx.putImageData(historyDeta[historyDeta.length - 1], 0, 0);
+//     historyDeta.pop()
+// };
